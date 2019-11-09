709cdf01f2a16aedc9821c76610dfeec90b2ddec
Pavel Schoffer <xschof00@gmail.com>
2019-11-09 20:23:38 +0100

    server is up

---
+++ server/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Elli Server',
+    version='1.0',
+    long_description=__doc__,
+    packages=['Elli'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask-cors==3.0.8',
+        'flask',
+    ]
+)
