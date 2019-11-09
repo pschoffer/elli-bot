5fa3ae84057d6f3567875acb8393e54edfde2beb
Pavel Schoffer <xschof00@gmail.com>
2019-11-09 20:34:05 +0100

    adding index

--- server/app.py
+++ server/app.py
@@ -4,13 +4,15 @@ from flask_cors import CORS
 from flask.views import MethodView
 
 
-class WebView(MethodView):
-    def get(self):
-        return "Sup!"
-
-
 app = Flask(__name__)
 CORS(app)
 
+
+class WebView(MethodView):
+    def get(self):
+        return app.send_static_file('index.html')
+
+
 app.add_url_rule('/', view_func=WebView.as_view("web"))
+
 app.run(host='0.0.0.0')
