e63c01efaa435821d7966b1863c9a89c44124829
Pavel Schoffer <xschof00@gmail.com>
2019-11-09 22:21:02 +0100

    websites queries status

--- server/app.py
+++ server/app.py
@@ -13,6 +13,14 @@ class WebView(MethodView):
         return app.send_static_file('index.html')
 
 
+class StatusView(MethodView):
+    def get(self):
+        return {
+            "status": "NOT_CONNECTED"
+        }
+
+
 app.add_url_rule('/', view_func=WebView.as_view("web"))
+app.add_url_rule('/status', view_func=StatusView.as_view("status"))
 
 app.run(host='0.0.0.0')
