c088865b61a77c5b25c8b5f998c6bed0baef44e7
Pavel Schoffer <xschof00@gmail.com>
2019-11-10 19:31:13 +0100

    frontend sends instructions

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask.views import MethodView
 import socket
@@ -54,7 +54,15 @@ class StatusView(MethodView):
         return response
 
 
+class InstructionView(MethodView):
+    def post(self):
+        logging.info("Instruction request recieved '%s'", request.json)
+        return "OK"
+
+
 app.add_url_rule('/', view_func=WebView.as_view("web"))
 app.add_url_rule('/status', view_func=StatusView.as_view("status"))
+app.add_url_rule(
+    '/instruction', view_func=InstructionView.as_view("instruction"))
 
 app.run(host='0.0.0.0')
