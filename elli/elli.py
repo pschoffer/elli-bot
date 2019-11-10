226ab3e468f4ac5b03e64e0a0e75e9e183cc96ce
Pavel Schoffer <xschof00@gmail.com>
2019-11-10 22:04:57 +0100

    client basic socket stuff

--- elli/elli.py
+++ elli/elli.py
@@ -1,15 +1,44 @@
 import socket
 import logging
+import time
 
 SERVER_IP = "192.168.0.102"
 SERVER_PORT = 5005
 
 logging.basicConfig(level=logging.INFO)
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.connect((SERVER_IP, SERVER_PORT))
-logging.info("Connected to a server %s:%s", SERVER_IP, SERVER_PORT)
 
-while(True):
-    data = serverSocket.recv(1024)
-    logging.info("Recieved '%s'", data)
+class Communicator:
+    def connect(self):
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            logging.info("Trying to connect to a server %s:%s",
+                         SERVER_IP, SERVER_PORT)
+            try:
+                self.serverSocket.connect((SERVER_IP, SERVER_PORT))
+                break
+            except ConnectionRefusedError:
+                time.sleep(5)
+            pass
+        logging.info("Connected to a server!")
+
+    def listen(self):
+        eofRecieved = False
+        while not eofRecieved:
+            data = self.serverSocket.recv(1024)
+            if data:
+                logging.info("Got data: %s", data)
+            else:
+                eofRecieved = True
+                logging.info("Reached end of connection.")
+
+    def cleanup(self):
+        logging.info("Cleaning up socket")
+        self.serverSocket.close()
+
+
+communicator = Communicator()
+while True:
+    communicator.connect()
+    communicator.listen()
+    communicator.cleanup()
