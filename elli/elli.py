41c933e8540ab0189ee2ece85a6f8623fc0eeb3d
Pavel Schoffer <xschof00@gmail.com>
2019-11-24 18:26:55 +0100

    starting motion

--- elli/elli.py
+++ elli/elli.py
@@ -3,18 +3,25 @@ import logging
 import time
 import json
 from subprocess import call
-from gpiozero import LED, Button
+from datetime import datetime
+from gpiozero import LED, Button, Motor
 
 SERVER_IP = "192.168.0.102"
 SERVER_PORT = 5005
+MOTION_STOP_MICROS = 2000
 
 logging.basicConfig(level=logging.INFO)
 
 statusLed = LED(24)
 killButton = Button(21)
+motorLeft = Motor(4, 3)
+motorRight = Motor(16, 20)
 
 
 class Communicator:
+    def __init__(self, motionController):
+        self.motionController = motionController
+
     def connect(self):
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while True:
@@ -23,7 +30,7 @@ class Communicator:
             try:
                 self.serverSocket.connect((SERVER_IP, SERVER_PORT))
                 break
-            except ConnectionRefusedError:
+            except (ConnectionRefusedError, TimeoutError):
                 time.sleep(5)
             pass
         logging.info("Connected to a server!")
@@ -36,7 +43,7 @@ class Communicator:
                 logging.info("Got data: %s", data)
                 instruction = json.loads(data.decode("utf-8"))
                 direction = instruction["direction"]
-                logging.info("Decoded to: %s", direction)
+                motionController.setInstruction(direction)
             else:
                 eofRecieved = True
                 logging.info("Reached end of connection.")
@@ -50,11 +57,47 @@ def shutdown():
     logging.info("Shutting Down!!")
     statusLed.blink(0.1, 0.1)
     call("sudo shutdown -h now", shell=True)
-
-
 killButton.when_pressed = shutdown
 
-communicator = Communicator()
+
+class MotionController:
+    _opositeDirections = frozenset([
+        frozenset(["straight", "back"]),
+        frozenset(["left", "right"])
+    ])
+
+    def __init__(self, motorLeft, motorRight):
+        self.motorLeft = motorLeft
+        self.motorRight = motorRight
+        self._setInstructionTime()
+        self.currentInstruction = None
+        self.nextInstruction = None
+        
+    def setInstruction(self, instruction):
+        isOposite = self.isOpositeToCurrentInstruction(instruction)
+        logging.info("Setting operation %s [oposite: %s]", instruction, isOposite)
+        self.currentInstruction = instruction
+
+    def isOpositeToCurrentInstruction(self, instruction):
+        thisCombination = frozenset([self.currentInstruction, instruction])
+        return thisCombination in MotionController._opositeDirections
+
+    def _setInstructionTime(self):
+        self.lastInstructionTime = datetime.now()
+
+    def _forward(self):
+        self.motorLeft.forward()
+        self.motorRight.forward()
+        self._setInstructionTime()
+
+    def _backward(self):
+        self.motorLeft.backward()
+        self.motorRight.backward()
+        self._setInstructionTime()
+
+
+motionController = MotionController(motorLeft, motorRight)
+communicator = Communicator(motionController)
 while True:
     statusLed.blink(0.5, 0.5)
     communicator.connect()
